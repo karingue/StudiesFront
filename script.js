789ec7c04372ca160a52e7b25d568bf9a76c4812
Gabriel Karing <gabhenkar@gmail.com>
2024-11-26 15:00:30 -0300

    uses att commit
    Some uses of the study atualized

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-const checkbox = document.getElementById("myCheckbox");
-const textInput = document.getElementById("myText");
-
-// Habilita ou desabilita a escrita no campo de texto
-checkbox.addEventListener("change", () => {
-    textInput.disabled = !checkbox.checked;
-});
